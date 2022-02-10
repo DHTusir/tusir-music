8b7efff15f4b91865f63d415ba79b31ea427a006
Tusir <tuwuei@gmail.com>
2022-02-10 14:10:59 +0800

    first commit

---
+++ src/main/java/com/tusir/tusirmusic/TusirMusicApplication.java
@@ -0,0 +1,13 @@
+package com.tusir.tusirmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusirMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TusirMusicApplication.class, args);
+    }
+
+}
